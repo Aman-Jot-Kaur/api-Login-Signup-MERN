0cf959b537471b642ea13f0ec9e58d7b2a0bb571
Amanjot Kaur <amritanarang6@gmail.com>
2023-08-02 17:41:16 +0530

    removed not used code

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const connectionUrl = "mongodb://0.0.0.0:27017/LoginSignup";
-
 app.use("/", require("./routes/userRoutes"));
 
 const { MONGODB_CONNECTION } = process.env;
 console.log(MONGODB_CONNECTION);
 mongoose
-  .connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((result) => console.log("connected to mongo"))
   .catch((err) => console.log(err));
 
